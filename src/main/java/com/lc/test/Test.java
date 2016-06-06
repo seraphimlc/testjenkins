94a7c86e9439d5d693a94a7ad3c3240e3eafe3f9
zhanming.lc <zhanming.lc@alibaba-inc.com>
2016-06-06 16:57:58 +0800

    package

---
+++ src/main/java/com/lc/test/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by liuchang on 16/6/6.
+ */
+package com.lc.test;
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test.main");
+        System.out.println("args = " + args);
+    }
+}
